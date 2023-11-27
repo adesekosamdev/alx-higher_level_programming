5178a4f687fa12ffaf932a770215d800003430e7
Pizza Man <adesekosamdev@gmail.com>
2023-11-27 19:53:46 +0000

    add new tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
+
 int check_cycle(listint_t *list)
 {
 	listint_t *tempnorm, *tempfwd;
@@ -10,11 +17,11 @@ int check_cycle(listint_t *list)
 		tempfwd = list->next;
 		while (tempnorm != NULL && tempfwd != NULL && tempfwd->next != NULL)
 		{
+			tempnorm = tempnorm->next;
+			tempfwd = tempfwd->next->next;
 			if (tempnorm == tempfwd)
 			return (1);
 		}
-		tempnorm = tempnorm->next;
-		tempfwd = tempfwd->next->next;
 	}
 	return (0);
 }
