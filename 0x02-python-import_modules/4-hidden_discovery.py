27d57b2758b6905017babcb430715ca7329ee88e
Pizza Man <adesekosamdev@gmail.com>
2023-12-01 05:09:48 +0000

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,10 +3,9 @@ import hidden_4
 
 
 def main():
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
 
 
 if __name__ == "__main__":
