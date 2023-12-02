66f4d3e0a7d0a6494f7d978a83c39586c0108800
Pizza Man <adesekosamdev@gmail.com>
2023-12-02 20:17:04 +0000

    update

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list:
         max = 0
-        for i in range(0, list_len):
+        for i in range(0, len(my_list)):
             if my_list[i] > max:
                 max = my_list[i]
         return max
