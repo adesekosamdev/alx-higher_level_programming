d38b1dbfae6e77e977b6d4b5345504bf82697ddd
Pizza Man <adesekosamdev@gmail.com>
2023-12-03 10:08:23 +0000

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,6 +2,11 @@
 #include <object.h>
 #include <listobject.h>
 
+/**
+* print_python_list_info - prints some basic info about pytohn list
+* @p: python object
+**/
+
 void print_python_list_info(PyObject *p)
 {
 	int i;
@@ -9,6 +14,6 @@ void print_python_list_info(PyObject *p)
 
 	printf("[*] Size of the Python List = %li\n", PyList_Size(p));
 	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < PyList_Size(p); i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 }
