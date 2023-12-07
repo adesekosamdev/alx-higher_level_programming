8422dd36db311ae7fb48c959c48d858e7e6ea5f8
Pizza Man <adesekosamdev@gmail.com>
2023-12-07 04:34:10 +0000

    update 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     if a_dictionary is not None:
-        length = len(list(my_dict.keys()))
-        return length
+        return len(a_dictionary)
     return None
