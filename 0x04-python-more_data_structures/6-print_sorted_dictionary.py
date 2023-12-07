f7987c1e8f57669ddbdcf5dcbe256ee1d036ccb6
Pizza Man <adesekosamdev@gmail.com>
2023-12-07 04:50:22 +0000

    update

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is not None:
-        keys = sorted(a_dictionary.keys())
-        for k in keys:
-            print("{}: ".format(k), end='')
-            print(a_dictionary.get(k))
+    for k in sorted(a_dictionary.keys()):
+        print('{}: {}'. format(k, a_dictionary[k]))
--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    tuple_hold = {key: value}
+    a_dictionary.update(tuple_hold)
     return a_dictionary
