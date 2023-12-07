d37269eecc5f8adabaed198e16e8b0b11080b928
Pizza Man <adesekosamdev@gmail.com>
2023-12-07 04:55:13 +0000

    update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary is not None:
-        hold = a_dictionary.copy()
-        for k, v in sorted(hold.items()):
-            value = value * 2
-            tuple_hold = {k: value}
-            hold.update(tuple_hold)
-        return hold
+    return {key: val * 2 for key, val in a_dictionary.items()}
