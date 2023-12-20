fbeac93da735236779e70f0ed00b5b1eb1d37830
Pizza Man <adesekosamdev@gmail.com>
2023-12-20 02:46:18 +0000

    update

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" singly linked list"""
+
+
+class Node:
+    """class Node that defines a node of a singly linked list"""
+    def __init__(self, data, next_node=None):
+        """Instantiation with size"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """retrieves data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """sets data"""
+        if type(value) is not int:
+            raise TypeError('data must be an integer')
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """retrieves next node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """sets next node"""
+        if value is not None and type(value) is not Node:
+            raise TypeError('next_node must be a Node object')
+        else:
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+        self.linked_list = []
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        new_node.next_node = self.__head
+        self.__head = new_node
+        self.linked_list = []
+        temp = Node(self.__head.data, self.__head.next_node)
+        while temp is not None:
+            self.linked_list.append(temp.data)
+            temp = temp.next_node
+        self.linked_list.sort()
+
+    def __str__(self):
+        return '\n'.join(list(map(str, self.linked_list)))
