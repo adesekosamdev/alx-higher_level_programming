ea2963b4d0b7fb472c47e0444c6763e93d7a2092
Pizza Man <adesekosamdev@gmail.com>
2024-02-25 13:41:25 +0000

    update tasks

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -59,3 +59,11 @@ class Rectangle:
             return ""
         return ("\n".join(["".join(["#" for i in range(self.__width)])
                 for j in range(self.__height)]))
+    
+    def __repr__(self):
+        """ returns the rectangle with the character #
+        """
+        if self.__width is 0 or self.__height is 0:
+            return ""
+        return ("\n".join(["".join(["#" for i in range(self.__width)])
+                for j in range(self.__height)]))
