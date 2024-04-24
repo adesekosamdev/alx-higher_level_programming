3503538e874c6053df9fa7000ba515bec46f9bbc
Pizza Man <adesekosamdev@gmail.com>
2024-04-24 04:39:04 +0000

    update tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
 const episode = process.argv[2];
 const url = 'http://swapi.co/api/films/' + episode;
-const request = require('request');
-request(url, function (err, response, body) {
-  if (err) {
-    throw err;
-  } else {
-    console.log(JSON.parse(body).title);
-  }
-}
-);
+request(url, function (error, response, body) {
+	console.log(error || JSON.parse(body).title);
+});
